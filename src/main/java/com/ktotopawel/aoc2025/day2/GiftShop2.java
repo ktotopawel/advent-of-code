f891171791c89ac2837997e470a80a3457fb1378
ktotopawel <pawel.juszczak02@gmail.com>
2025-12-03 16:10:22 +0100

    day 2 problem 2 completed

--- src/main/java/com/ktotopawel/aoc2025/day2/GiftShop2.java
+++ src/main/java/com/ktotopawel/aoc2025/day2/GiftShop2.java
@@ -1,5 +1,28 @@
 package com.ktotopawel.aoc2025.day2;
 
-public class GiftShop2 {
+import com.ktotopawel.aoc2025.Utils;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class GiftShop2 {
+    public static void main(String[] args) {
+        long ans = 0;
+
+        String input = Utils.readString("day2.txt");
+        List<Range> parsedRecords = Range.fromInput(input);
+
+        for (Range record : parsedRecords) {
+            for (long i = record.start(); i <= record.end(); i++) {
+                String parsedString = String.valueOf(i);
+
+                if ((parsedString + parsedString).indexOf(parsedString, 1) != parsedString.length()) {
+                    ans += i;
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
 }
